161ec642179509ae21420cd608954c3eef0e4cf9
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-06 00:18:07 +0700

    feat: Add user registration endpoint

---
+++ server/api/auth/register.post.js
@@ -0,0 +1,79 @@
+import prisma from "~/utils/prisma.js";
+import bcrypt from "bcrypt";
+import {userRegisterValidation} from "~/server/validation/userRegisterValidaton.js";
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+
+    // Validate user input
+    const {error, value} = userRegisterValidation(body);
+    if (error) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: error.details[0].message
+        });
+    }
+
+    const {name, email, password, referrerCode} = value;
+
+    // Check if email already exists
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (existingUser) {
+        throw createError({
+            statusCode: 409,
+            statusMessage: "Email already exists"
+        });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Logic for handling referrer code
+    let referredById = null;
+    if (referrerCode) {
+        const referrer = await prisma.user.findUnique({
+            where: {
+                referralCode: referrerCode
+            }
+        });
+
+        if (referrer) {
+            referredById = referrer.id;
+        } else {
+            throw createError({
+                statusCode: 400,
+                statusMessage: "Invalid referrer code"
+            });
+        }
+    }
+
+    // Create a new user
+    try {
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+                role: "INVESTOR",
+                ...(referredById && {
+                    referredById: referredById
+                })
+            }
+        });
+    } catch (error) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: "Failed to create user"
+        });
+    }
+
+    // Send success response
+    event.node.res.statusCode = 201;
+    return {
+        message: "User created successfully"
+    };
+});
