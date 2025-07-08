90913390b63b17d87a0568c85bde3d42a97c3d8c
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-08 22:38:10 +0700

    test(user): Add register user test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,27 @@
-import supertest from "supertest";
-import {removeTestUser} from "~/tests/test-util.js";
+import {removeTestUser} from "./test-util.js";
+import {api} from "./api.js";
+import {logger} from "../utils/logger.js";
 
-describe("POST /api/auth/register", () => {
-    afterEach(async ()=>{
+describe('POST /api/auth/register', () => {
+
+    afterEach(async () => {
         await removeTestUser();
     });
 
-    it("Should create a new user", async()=>{
-        const response = await supertest(web);
-    })
+    it("Should create a test user", async () => {
+        const response = await api.post("/api/auth/register")
+            .send({
+                name: "Test User",
+                email: "test@me.com",
+                password: "password1"
+            })
+            .expect(201);
+
+        logger.info(response.body.message);
+        logger.info(JSON.stringify(response.body.data, null, 2));
+
+        expect(response.status).toBe(201);
+        expect(response.body.success).toBe(true);
+        expect(response.body.data.newUser.email).toBe('test@me.com');
+    });
 })
