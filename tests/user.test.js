47a12d9a43a28cd23907b3d3f7b1405655affab5
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-10 15:51:48 +0700

    feat(user): Add user login tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,15 @@
-import {removeTestUser} from "./test-util.js";
+import {createTestUser, removeTestUser} from "./test-util.js";
 import {api} from "./api.js";
 import {logger} from "../utils/logger.js";
 
+/**
+ * User Registration Tests
+ */
 describe('POST /api/auth/register', () => {
 
-    // afterEach(async () => {
-    //     await removeTestUser();
-    // });
+    afterEach(async () => {
+        await removeTestUser();
+    });
 
     it("Should create a test user", async () => {
         const response = await api.post("/api/auth/register")
@@ -92,5 +95,66 @@ describe('POST /api/auth/register', () => {
         expect(response.status).toBe(400);
         expect(response.body.success).toBeUndefined();
         expect(response.body.message).toBe('Password must be at least 8 characters long and contain at least one letter and one number');
+    });
+})
+
+/**
+ * User Login Tests
+ */
+describe('POST /api/auth/login', () => {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it("Should login a user", async () => {
+        const response = await api.post("/api/auth/login")
+            .send({
+                email: "testbudi@investor.com",
+                password: "password123"
+            })
+            .expect(200);
+
+        logger.info(response.body);
+        logger.info(JSON.stringify(response.body.data, null, 2));
+
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+        expect(response.body.data.user.email).toBe('testbudi@investor.com');
+    });
+
+    it("Should not login a user with invalid email", async () => {
+        const response = await api.post("/api/auth/login")
+            .send({
+                email: "test",
+                password: "password123"
+            })
+            .expect(400);
+
+        logger.info(response.body);
+        logger.info(JSON.stringify(response.body.data, null, 2));
+
+        expect(response.status).toBe(400);
+        expect(response.body.success).toBeUndefined();
+        expect(response.body.message).toBe('Invalid email or password');
+    });
+
+    it("Should not login a user with invalid password", async () => {
+        const response = await api.post("/api/auth/login")
+            .send({
+                email: "testbudi@investor.com",
+                password: "pass"
+            })
+            .expect(401);
+
+        logger.info(response.body);
+        logger.info(JSON.stringify(response.body.data, null, 2));
+
+        expect(response.status).toBe(401);
+        expect(response.body.success).toBeUndefined();
+        expect(response.body.message).toBe('Invalid email or password');
     });
 })
